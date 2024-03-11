ca5728a3e9edbc75a48f95778c77f207c578da00
Maria <lady.mescheryakowa@yandex.ru>
2024-03-11 23:45:31 +0300

    шрифты

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 import { pug_2_html } from "./tasks/pug2html.js";
 import { config } from "./gulp-config.js";
+import {fontsTTF2WOFF} from "./tasks/fontsTTF2WOFF.js";
 
 const { series } = config.gulp;
 
-const build = series(pug_2_html);
+const build = series(pug_2_html, fontsTTF2WOFF);
 export default series(build)
