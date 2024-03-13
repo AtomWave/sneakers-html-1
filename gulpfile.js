3d635764d2e294c7c28d98b9dc0c1cf0b24ab596
Tatiana B <tbeketova04@gmail.com>
2024-03-13 19:23:26 +0200

    устанавливает плагин sharp, создает папку source/image

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 import { pug_2_html } from "./tasks/pug2html.js";
 import { config } from "./gulp-config.js";
+import {optimizeImages} from "./tasks/optimizeRastr.js";
 
 const { series } = config.gulp;
 
 const build = series(pug_2_html);
-export default series(build)
+export default series(build);
+
+
+const img = series(optimizeImages);
+export {img};
+
