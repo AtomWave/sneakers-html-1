a3ccdb5f21da2df7c2f0dba6596b7679b81d833d
Andrew Chernigovsky <chernigovsky108@gmail.com>
2024-03-18 15:32:57 +0300

    fixes

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ import { pug2html, watcherPug } from './tasks/pug2html.js'
 import { config } from './gulp-config.js'
 import { server } from './tasks/browserSyns.js'
 
-import { resizeImage } from "./tasks/sharp.js";
-import { fontsTTF2WOFF } from "./tasks/fontsTTF2WOFF.js";
+import { resizeImage } from './tasks/sharp.js';
+import { fontsTTF2WOFF } from './tasks/fontsTTF2WOFF.js';
 
 const { series } = config.gulp;
 
-const build = series(pug2html, fontsTTF2WOFF, resizeImage);
+const build = series(pug2html, fontsTTF2WOFF);
 
 export default series(
   build,
--- tasks/pug2html.js
+++ tasks/pug2html.js
@@ -1,30 +1,26 @@
 import { config } from './../gulp-config.js'
 
-<<<<<<< HEAD
 const { pug_2_html, plumber_watch, browser_2_server } = config.tasks
-=======
-const { pug2HTML, plumberWatch, browserServer } = config.tasks
->>>>>>> e5f9b5262ea5c3451e3746274e6d7f2f1500512f
 const { src, dest, watch, series } = config.gulp
 const { build, pug } = config.paths
 
-function pagesPug () {
+function pagesPug() {
   return (
     src(`${pug.pages}`)
-      .pipe(plumberWatch())
-      .pipe(pug2HTML({ pretty: true }))
+      .pipe(plumber_watch())
+      .pipe(pug_2_html({ pretty: true }))
       .pipe(dest(`${build}pages`))
-      .pipe(browserServer.stream())
+      .pipe(browser_2_server.stream())
   )
 };
 
-function mainPug () {
+function mainPug() {
   return (
     src(`${pug.main}`)
-      .pipe(plumberWatch())
-      .pipe(pug2HTML({ pretty: true }))
+      .pipe(plumber_watch())
+      .pipe(pug_2_html({ pretty: true }))
       .pipe(dest(`${build}`))
-      .pipe(browserServer.stream())
+      .pipe(browser_2_server.stream())
   )
 };
 
@@ -35,9 +31,5 @@ export const pug2html = (done) => {
 }
 
 export const watcherPug = () => {
-<<<<<<< HEAD
   watch(`${pug.all}**/*.pug`).on('change', series(pug2html, browser_2_server.reload))
-=======
-  watch(`${pug.all}**/*.pug`).on('change', series(pug2html, browserServer.reload))
->>>>>>> e5f9b5262ea5c3451e3746274e6d7f2f1500512f
 }
