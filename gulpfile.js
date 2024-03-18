e5f9b5262ea5c3451e3746274e6d7f2f1500512f
Izosimov_Andrew <chernigovsky108@gmail.com>
2024-03-18 15:21:42 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,15 @@ import { pug2html, watcherPug } from './tasks/pug2html.js'
 import { config } from './gulp-config.js'
 import { server } from './tasks/browserSyns.js'
 
-<<<<<<< HEAD
-import { optimizeImages } from './tasks/optimizeRastr.js'
-import { fontsTTF2WOFF } from './tasks/fontsTTF2WOFF.js'
-
-const { series } = config.gulp
-const img = series(optimizeImages)
-
-const build = series(pug2html, fontsTTF2WOFF)
-=======
-import { resizeImage, watchImg } from "./tasks/sharp.js";
+import { resizeImage } from "./tasks/sharp.js";
 import { fontsTTF2WOFF } from "./tasks/fontsTTF2WOFF.js";
 
 const { series } = config.gulp;
 
 const build = series(pug2html, fontsTTF2WOFF, resizeImage);
->>>>>>> ac48b5afac0f029b6f8a39d67d422d839e793e0d
 
 export default series(
   build,
   server,
-  watcherPug,
-  watchImg
+  watcherPug
 )
