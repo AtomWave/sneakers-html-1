23148ad8ed72539e5d9c973649c1aeb2fd24c8d9
Andrew Chernigovsky <chernigovsky108@gmail.com>
2024-03-19 22:16:27 +0300

    fix

--- tasks/sharpImages/findCatalogs.js
+++ tasks/sharpImages/findCatalogs.js
@@ -1,39 +1,53 @@
 import { readdir } from 'fs/promises'
-import { resolve, join } from 'path'
+import { join } from 'path'
+import fs from 'node:fs'
+
+const source = 'raws'
 
 async function getFiles(dir) {
-  const files = await readdir(dir, { withFileTypes: true })
-  const output = files.map((file) => join(dir, file.name))
+  let output = []
+  if (fs.lstatSync(dir).isDirectory()) {
+    let files = await readdir(dir, { withFileTypes: true })
+    let file = files.map((file) => join(dir, file.name))
+    output.push(file)
+  } else {
+    let files = await readdir(source, { withFileTypes: true })
+    let file = files.map((file) => join(source, file.name))
+    output.push(file)
+  }
   return output
 }
 
 let paths = []
 let files = []
 
-const getDirectories = async (source) => {
-  const dirents = await readdir(source, { withFileTypes: true })
-  const directories = dirents
-    .filter((dirent) => dirent.isDirectory())
-    .map((dirent) => join(source, dirent.name))
-  paths.push(...directories)
+let regExestentions = /.png|.jpg|.jpeg/gm
+
+async function populatePaths() {
+  try {
+    const dirents = await readdir(source, { withFileTypes: true })
+    const directories = dirents.filter(dirent => dirent.isDirectory()).map(dirent => join(source, dirent.name))
+    const rootFiles = dirents.filter(dirent => dirent.isFile()).map(dirent => join(source, dirent.name))
+    console.log(rootFiles, 'rootFiles');
+    paths.push(...directories, ...rootFiles)
+    console.log(paths, 'paths')
+    await processDirectories()
+  } catch (err) {
+    // console.error(err)
+  }
 }
 
 async function processDirectories() {
   for (const path of paths) {
+    // if (path.match(regExestentions)) {
+    //   console.log(path, 'path');
+    //   const filesInDir = await getFiles(path)
+    //   files.push(...filesInDir)
+    //   // console.log(files, 'FFF');
+    // }
+    console.log(path, 'path')
     const filesInDir = await getFiles(path)
     files.push(...filesInDir)
-  }
-}
-
-async function populatePaths() {
-  const source = 'raws'
-  try {
-    const dirents = await readdir(source, { withFileTypes: true })
-    const directories = dirents.filter(dirent => dirent.isDirectory()).map(dirent => join(source, dirent.name))
-    paths.push(...directories)
-    await processDirectories()
-  } catch (err) {
-    console.error(err)
   }
 }
 
@@ -44,10 +58,11 @@ async function allPaths() {
 let reg = /\\/g;
 export let arr = [];
 export let getAllPaths = allPaths();
+
 getAllPaths.then(res => {
 
-  let arr1 = res.map(el => el.replace(reg, '/'))
-  arr.push(...arr1);
+  // let arr1 = res.map(el => el.replace(reg, '/'))
+  arr.push(...res);
   return arr
 }).then(file => {
   console.log(file, 'file');
