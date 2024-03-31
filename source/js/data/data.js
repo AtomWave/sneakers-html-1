5dd9820dc9322dd17d670e8b957fba64b04c9a4d
Andrew Chernigovsky <chernigovsky108@gmail.com>
2024-03-31 22:31:09 +0300

    js

---
+++ source/js/data/data.js
@@ -0,0 +1,15 @@
+
+export const data = [
+  {
+    name: "Z,ion Williamson x Air Jordan 1 Retro Low OG 'Voodoo'",
+    brend: "Nike",
+    id: 1,
+    cost: 27590,
+    oldCost: 29890,
+    amount: null,
+    sizes: [],
+    images: ['https://ir-1.ozone.ru/s3/multimedia-1-u/wc1000/6921399558.jpg'],
+    choose: false,
+    status: true,
+  }
+]
