8c6fd9bae2eb740aff2b96b84de886d9c6ee8d46
xfir664 <xfir664@yandex.ru>
2024-04-21 15:21:46 +0300

    #IT-26 Разбивает auth.js на модули auth.js и registration.js

--- source/js/modules/auth/auth.js
+++ source/js/modules/auth/auth.js
@@ -1,49 +1,11 @@
-const registrationButton = document.querySelector('.header-navigation__registration-button');
+import { uploadImg, onSubmitForm } from "./registration";
+
 const registrationForm = document.querySelector('.registration-modal');
+const registrationButton = document.querySelector('.header-navigation__registration-button');
 const usersProfile = document.querySelector('.user-navigation-header');
 const imageUploadButton = registrationForm.querySelector('.registration-modal__input-image-upload');
-const usersImagePreviews = registrationForm.querySelector('.registration-modal__image-preview');
-
-const MAX_IMAGE_SIZE = 2000000;
-
-const reader = new FileReader();
-
-function uploadImg () {
-  const file = imageUploadButton.files[0];
-  if(file.size > MAX_IMAGE_SIZE) {
-    registrationForm.querySelector('.registration-modal__error-mesage').textContent = 'Размер файла не должен привышать 2 Мб';
-    registrationForm.querySelector('.registration-modal__error-mesage').classList.remove('hidden');
-  } else {
-    registrationForm.querySelector('.registration-modal__error-mesage').classList.add('hidden');
-    reader.addEventListener('load', onImageUpload)
-    reader.readAsDataURL(file);
-  }
-}
-
-function onImageUpload () {
-  usersImagePreviews.src = reader.result;
-}
-
-
-function onSubmitForm (evt) {
-    evt.preventDefault();
-    const userName = registrationForm.querySelector('#name').value;
-    const userEmail = registrationForm.querySelector('#email').value;
-    const userAvatarUrl = usersImagePreviews.src;
-    const userProfile = {
-        name: userName,
-        email: userEmail,
-        avatarUrl: userAvatarUrl
-    }
-    localStorage.setItem('userProfile', JSON.stringify(userProfile))
-    registrationForm.classList.add('hidden');
-    imageUploadButton.removeEventListener('change', uploadImg);
-    registrationForm.removeEventListener('submit', onSubmitForm);
-    checkLocalStorage();
-}
 
 function checkLocalStorage () {
-  registrationButton.addEventListener('click', onRegistrationButton);
   if(localStorage.getItem('userProfile') !== null) {
       registrationButton.classList.add('hidden');
       usersProfile.classList.remove('hidden');
@@ -62,7 +24,8 @@ function onRegistrationButton () {
 }
 
 function onWindowLoad () {
-    checkLocalStorage();
+  registrationButton.addEventListener('click', onRegistrationButton);
+  checkLocalStorage();
 }
 
-export { onWindowLoad }
+export { onWindowLoad, checkLocalStorage }
